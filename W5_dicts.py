df47af7f296650a1735e762f3c3689cdfc9815bc
AmnaJaved12 <amnajaved132001@gmail.com>
2023-03-20 16:19:26 +0000

    W5_dict

---
+++ W5_dicts.py
@@ -0,0 +1,27 @@
+#Initialise empty dictionary
+d = {}
+dictio = dict()
+#Initialise non-empty dictionary
+phonebook = {"Thomas":7765431783, "Aga":788792912, "MD":7777277723}
+#Print full dictionary
+print(phonebook)
+#Print/access individual elements
+name = input("Who you gonna call: ")
+if name in phonebook:
+    print(f"Calling...{phonebook[name]}")
+else:
+    print(f"No number for {name}")
+#Zipping two lists together to compose a dictionary
+names = ["Garry", "Ian", "Laura"]
+age = [56, 21, 16]
+people = dict(zip(names, age))
+print(people)
+#Traversing Dictionaries - accessing keys/values/both
+for thing in people:
+    print(thing)
+for thing in people.values():
+    print(thing)
+for thing in people.keys():
+    print(thing)
+for k, v in people.items():
+    print(f"Person {k} is {v} years old")
