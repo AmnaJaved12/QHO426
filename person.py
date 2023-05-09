2926af661273ab919bc88f033514ae3779a0e35b
AmnaJaved12 <amnajaved132001@gmail.com>
2023-05-09 11:32:50 +0100

    Week 9 Seminar

---
+++ person.py
@@ -0,0 +1,33 @@
+#The class Person which is a blue print for my object to store information about humans/people
+class Person:
+
+    #Class Attribute -> attribute/feature accessible/applied to
+    #every object of the class
+    MAX_W = 200
+
+
+    #Initializer/Constructor -> function that is automatically
+    #called when an object is instantiated (magic method)
+    #Initializer of any class has the same name
+    #"DUNDER" - Double Underscore
+    def __init__(self, name, age = 0, job = "", w=100):
+        #Below are the instance attributes (features)
+        self.name = name
+        self.age = age
+        self.job = job
+        if w > Person.MAX_W:
+            self.weight = Person.MAX_W
+        else:
+            self.weight = w
+
+p1 = Person("Luke", 23, "Clerk", 55)
+p2 = Person("Mary", 19, "Waitress", 48)
+p3 = Person("Udoh", job="HeadMaster")
+p4 = Person("Olu", w=600)
+
+print(f"Person 1 is called {p1.name} and Person 2 is called {p2.name}")
+print(f"Together their total weight is {p1.weight + p2.weight}")
+print(p1)
+print(p2)
+print(f"{p4.name} weights {p4.weight}")
+print(f"{p3.name} weights {p3.weight} and works as {p3.job}")
